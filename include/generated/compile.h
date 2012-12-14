06b2fcd1c538ae062090fcbfdf8fa745755161a8
Abhiman Kolte <koltebhau@gmail.com>
2012-12-14 11:39:05 +0530

    Fixed kernel memory leak through net sched 	modified:   net/sched/act_gact.c

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version 5 */
+/* This file is auto generated, version 6 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#5 PREEMPT Fri Dec 14 11:01:45 IST 2012"
-#define LINUX_COMPILE_TIME "11:01:45"
+#define UTS_VERSION "#6 PREEMPT Fri Dec 14 11:37:58 IST 2012"
+#define LINUX_COMPILE_TIME "11:37:58"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "koltebhau-amd64-phenomII-x6"
 #define LINUX_COMPILE_DOMAIN "(none)"
