edef5a42df0cbc5e350b4206e822fb9b186e144c
Abhiman Kolte <koltebhau@gmail.com>
2012-12-14 11:23:47 +0530

    Added improved memcopy driver 	new file:   include/linux/memcopy.h 	modified:   lib/Makefile 	new file:   lib/memcopy.c 	modified:   lib/string.c

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version 2 */
+/* This file is auto generated, version 5 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#2 PREEMPT Thu Dec 13 21:38:56 IST 2012"
-#define LINUX_COMPILE_TIME "21:38:56"
+#define UTS_VERSION "#5 PREEMPT Fri Dec 14 11:01:45 IST 2012"
+#define LINUX_COMPILE_TIME "11:01:45"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "koltebhau-amd64-phenomII-x6"
 #define LINUX_COMPILE_DOMAIN "(none)"
