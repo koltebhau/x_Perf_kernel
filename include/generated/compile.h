aeca0bb032f9423c0c24edb8cefe5773fb12e9d4
Abhiman Kolte <koltebhau@gmail.com>
2012-12-14 21:11:29 +0530

    die hard OC 	modified:   arch/arm/mach-msm/acpuclock-8x50.c

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version 8 */
+/* This file is auto generated, version 10 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#8 PREEMPT Fri Dec 14 20:35:58 IST 2012"
-#define LINUX_COMPILE_TIME "20:35:58"
+#define UTS_VERSION "#10 PREEMPT Fri Dec 14 21:07:42 IST 2012"
+#define LINUX_COMPILE_TIME "21:07:42"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "koltebhau-amd64-phenomII-x6"
 #define LINUX_COMPILE_DOMAIN "(none)"
