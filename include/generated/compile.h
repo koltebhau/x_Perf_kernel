c1e5ac82b3d17d0786d3569613ac80c7e5bcdbcc
Abhiman Kolte <koltebhau@gmail.com>
2012-12-15 20:20:08 +0530

    Enabled google snappy comp & decomp 	modified:   .config

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version 14 */
+/* This file is auto generated, version 3 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#14 PREEMPT Sat Dec 15 19:57:38 IST 2012"
-#define LINUX_COMPILE_TIME "19:57:38"
+#define UTS_VERSION "#3 PREEMPT Sat Dec 15 20:08:15 IST 2012"
+#define LINUX_COMPILE_TIME "20:08:15"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "koltebhau-amd64-phenomII-x6"
 #define LINUX_COMPILE_DOMAIN "(none)"
